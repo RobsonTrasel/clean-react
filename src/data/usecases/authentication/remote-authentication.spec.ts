80430bf4627a78e3291e10de0c0197210a84cc62
Robson Trasel <robson.trasel@gmail.com>
2022-12-16 21:34:01 -0300

    chore: setup for remote authentication test done

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,5 +1,20 @@
-describe('[Test] Remote Authentication', () => {
-  test('Should call HttpClient with the correct url', () => {
+import { RemoteAuthentication } from './remote-authentication';
+import { HttpPostClient } from '../../protocols/http/http-post-client';
 
+describe('[Test] Remote Authentication', () => {
+  test('Should be call HttpClient with the correct url', async () => {
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string
+      async post(url: string): Promise<void> {
+        this.url = url
+        return Promise.resolve()
+      }
+
+    }
+    const url = 'any_url'
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    await sut.auth()
+    expect(httpPostClientSpy.url).toBe(url)
   })
 })
